fa9d335b7551b41ef0ac776b687d8c8095848ee7
fifths <ligangmingx@gmail.com>
2018-03-01 15:46:51 +0800

    init

---
+++ src/config/WxPayConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'appid' => '',
+    'mch_id' => '',
+    'key' => '',
+    'app_secret' => '',
+    'sslcer_path' => '../cert/apiclient_cert.pem',
+    'sslkey_path' => '../cert/apiclient_key.pem',
+    'notify_url' => '',
+    'curl_proxy_host' => "0.0.0.0",
+    'curl_proxy_port' => 0,
+    'report_level' => 1
+);
